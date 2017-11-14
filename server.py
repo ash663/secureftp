ae68f533f993370c5e7d16d42071199b3990f8f4
Ashish <a.talk2ash@gmail.com>
2017-11-13 18:16:44 -0600

    Hashing and encryption portion added

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import os
 import socket
 import re
 import hashlib
+import time
 
 def incoming_connection(conn):
     #Send certificate
@@ -20,6 +21,19 @@ def incoming_connection(conn):
         if re.search('download', request):
             fileName=request.split(':')[1]
             #add code to check if file exists
+            if os.path.isfile(fileName):
+                #File exists. Now encrypt the file.
+                #Encryption algo: Hash Timestamp. XOR with File. But, in blocks.
+                hashedTimeStamp = hmac.new(secret, msg=time.time(), digestmod=hashlib.sha1).hexdigest()
+                #msg is epoch time
+                with open(fileName, 'rb') as f:
+                    while True:
+                        data = f.read(BUF_SIZE)
+                        if not data:
+                            break
+                        encryptedFile = encryptedFile + (data ^ hashedTimeStamp)
+                encryptedFile = m.hexdigest()
+
         elif re.search('upload', request):
             fileName=request.split(':')[1]
         conn.sendall(reply)
@@ -27,16 +41,19 @@ def incoming_connection(conn):
     #came out of loop
     conn.close()
 
-#Used for hashing
+#Used for hashing and encrypting
 BUF_SIZE = 65536
-sha1 = hashlib.sha1()
+sha1 = hashlib.sha1() #Probably gotta change this, as not needed. Better to be done file-wise
+secret = 'abcdefghijkl' #Use shared secret key later
 #For opening a connection with client. Can make it work for multiple clients by select or threading
 PORT=4999
 PORT=PORT+1
 HOST=""
+#Create socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
+    #Bind socket to port
     s.bind((HOST, PORT))
 except socket.error as msg:
     print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
